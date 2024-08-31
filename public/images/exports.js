f96d3bc737b011cd057e57b360a968de2fdf0b2b
isaacgichuhi <zacmuturi45@gmail.com>
2024-08-31 19:01:38 +0300

    Added profile picture

--- public/images/exports.js
+++ public/images/exports.js
@@ -1,5 +1,5 @@
 export { default as c } from './copyright.svg';
-export { default as hero } from './smile.jpg';
+export { default as hero } from './isaac.jpeg';
 export { default as arrowdown } from './arrowdown.svg';
 export { default as insta } from './insta.svg';
 export { default as whatsapp } from './whatsapp.svg';
